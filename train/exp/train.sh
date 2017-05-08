bf5f28a772b77f24252d248aea7f9a2003e2cbdd
bearpaw <platero.yang@gmail.com>
2017-05-08 10:23:46 +0800

    modify the training scripts for multi-GPU training

--- train/exp/train.sh
+++ train/exp/train.sh
@@ -1,3 +1,5 @@
 cd ..
 
-CUDA_VISIBLE_DEVICES=0, th main.lua -netType hg-attention -expID crf_parts -nGPU 1 -LRNKer 11 -trainBatch 1
+# Example for training with 4x 12GB GPUs
+# Please refer to opts.lua for more training options
+CUDA_VISIBLE_DEVICES=0,1,2,3 th main.lua -netType hg-attention -expID crf_parts -nGPU 1 -LRNKer 11 -nGPU 4
