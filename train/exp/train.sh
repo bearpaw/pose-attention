a4d14d63cb8c2468b356b23ab6f14fb372f1bf97
bearpaw <platero.yang@gmail.com>
2017-05-08 10:30:33 +0800

    less nThreads

--- train/exp/train.sh
+++ train/exp/train.sh
@@ -1,5 +1,6 @@
 cd ..
 
 # Example for training with 4x 12GB GPUs
+# You may set larger -nThreads for faster data loading
 # Please refer to opts.lua for more training options
-CUDA_VISIBLE_DEVICES=0,1,2,3 th main.lua -netType hg-attention -expID crf_parts -nGPU 1 -LRNKer 11 -nGPU 4
+CUDA_VISIBLE_DEVICES=0,1,2,3 th main.lua -netType hg-attention -expID crf_parts -nGPU 1 -LRNKer 11 -nGPU 4 -nThreads 2
